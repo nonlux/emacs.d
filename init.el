10d0ab029252001b7831247d466037b24c8088ac
nonlux <nonluxi@gmail.com>
2016-12-11 01:27:15 +0800

    update js config

--- init.el
+++ init.el
@@ -121,12 +121,30 @@
 ;; modes
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
-(add-hook 'js2-mode-hook 'ac-js2-mode)
+(add-hook 'js2-jsx-mode-hook 'ac-js2-mode)
 (setq ac-js2-evaluate-calls t)
-(add-hook 'js2-mode-hook 'js2-refactor-mode)
+(add-hook 'js2-jsx-mode-hook 'js2-refactor-mode)
+(setq js2-mode-show-parse-errors nil)
 ;; (setq ac-js2-external-libraries '("full/path/to/a-library.js"))
 
+(defun my/use-eslint-from-node-modules () )
+(defun use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
 
+(add-hook 'flycheck-mode-hook 'use-eslint-from-node-modules)
+
+(defun flycheck-js ()
+;;  (flycheck-select-checker 'javascript-eslint)
+  (js2-mode-hide-warnings-and-errors)
+)
+(add-hook 'js2-jsx-mode-hook 'flycheck-js)
 
   (setq web-mode-markup-indent-offset 2
         web-mode-css-indent-offset 2
